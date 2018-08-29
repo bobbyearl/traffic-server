4e96b8bde7cbe630c54e7f10c3616e1fd7477bfc
Bobby Earl <bobby.earl@blackbaud.com>
2018-08-28 20:59:28 -0400

    Handling local + server.

--- server.js
+++ server.js
@@ -1,31 +1,53 @@
 const fs = require('fs-extra');
 const url = require('url');
 const http = require('http');
+const https = require('https');
 const httpProxy = require('http-proxy');
 
+const isProduction = !!process.env.WEBSITE_HOSTNAME;
 const PORT = process.env.port || 5050;
 const proxy = httpProxy.createProxyServer({});
-const server = http.createServer(function(req, res) {
 
-  const uri = url.parse(req.url, true);
+const requestHandler = (request, response) => {
+  const uri = url.parse(request.url, true);
 
   // Shortcircuit test
   if (uri.pathname === '/ping') {
-    return res.end('Traffic, nice.');
+    return response.end('Traffic, nice.');
   }
 
   const origin = decodeURIComponent(uri.query['origin']);
   const directory = decodeURIComponent(uri.query['directory']);
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Origin', '*');
 
   // Initial request, just change target to server.
   // All subsequent requests (come to root), change target to prefix.
-  proxy.web(req, res, { 
+  proxy.web(request, response, { 
     target: uri.pathname.indexOf('rtplive') > -1 ? origin : (origin + directory)
   });
-});
+};
 
-console.log(`Starting server on port ${PORT}`);
+// Azure doesn't need SSL
+const getServer = () => {
+
+  if (isProduction) {
+    return http.createServer(requestHandler);
+  }
+
+  return https.createServer(
+    {
+      key: fs.readFileSync('./ssl/localhost.key'),
+      cert: fs.readFileSync('./ssl/localhost.crt'),
+      requestCert: false,
+      rejectUnauthorized: false
+    },
+    requestHandler
+  );
+};
+
+const server = getServer();
+
 server.listen(PORT, () => {
-  console.log(`Traffic server running on port ${PORT}`);
+  console.log('Production:', isProduction);
+  console.log(server.address());
 });
